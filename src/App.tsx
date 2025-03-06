8f905b03c24df0bd783b8cba6fdc9c15a77116c3
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-03-06 13:48:25 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  let slowSpin = "animate 2s linear";
+  const formItems = [{id: 1, label: "First Name"}, {id: 2, label: "Last Name"}, {id: 3, label: "Email"},  {id: 4, label: "Date of Birth"}];
+  return (
+  <div className="flex h-screen bg-blue-100 items-start p-5">
+    <div className="flex flex-row gap-2 flex-grow-1 w-500 items-center mx-auto bg-white shadow-lg rounded-xl pl-5 pr-12 py-5">
+      <img src={logo} className="animate-spin flex-shrink-1 flex-grow-0 w-16" alt="logo" />
+      <h1 className="ml-5 text-center text-xl font-medium flex-grow-1">Welcome to Lesson 5 of $react-typescript with <br/> #tailwindcss</h1>
+      <div className="flex flex-col gap-1">
+      {
+        formItems.map(item => {
+          return (
+            <React.Fragment key={item.id}>
+              <label htmlFor={item.label}>{item.label}</label>
+              <input type="text" />
+            </React.Fragment>
+          )
+        })
+      }
+      </div>
+    </div>
+  </div>
+  );
+
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const Heading = ({name} : {name: string}) => React.createElement(
+  "h1",
+  {className: "bg-blue-500 text-center p-4"},
+  "Hi, welcome to this new page",
+  React.createElement("i", {}, `${name}`),
+  React.createElement("a", {href: "https://google.com", className: "text-green-100"}, "Don't look too hard!"),
+)
+
+const BannerDiv = ({name} : {name: string}) => {
+  return (
+    <div id="banner" className='bg-teal-700 text-center p-4'>
+      <h1 id="headingOne" className='text-gray-100 text-2xl'>Hello again</h1>
+      <i>Welcome {name}</i>
+      <br></br><a href='https://google.com' className='text-green-300'> Don't look too hard</a>
+    </div>
+  )
+}
+
+root.render(
+  <React.StrictMode>
+    <BannerDiv name="Abhinav"/>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

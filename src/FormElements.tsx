a2a598b488f2a112c79cc181a2846049778b1dba
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-03-07 11:28:53 +0530

    Change minor dead tailwind class

--- src/FormElements.tsx
+++ src/FormElements.tsx
@@ -10,7 +10,7 @@ export default function FormElements({formItems} : {
               return (
                 <React.Fragment key={item.id}>
                   <label className="text-m font-medium my-2" htmlFor={item.label}>{item.label}</label>
-                  <input className="border border-2 border-grey-200 rounded-md p-2" id={item.label} type={item.type} />
+                  <input className="border-2 border-grey-200 rounded-md p-2" id={item.label} type={item.type} />
                 </React.Fragment>
               )
             })

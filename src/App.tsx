af391643bc02def7b1b169547b9c1c5add764e66
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-03-07 11:25:57 +0530

    Follow lesson one of part two

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppContainer from './AppContainer';
 import FormHeading from './FormHeading';
 import FormElements from './FormElements';
 import FormSubmit from './FormSubmit';
+import { FormBtn } from './FormBtn';
+import FormHome from './FormHome';
+
 function App() {
+  const [formOpen, setFormOpen] = useState("HOME");
   const formItems = [{id: 1, label: "First Name", type: "text"}, {id: 2, label: "Last Name", type: "text"}, {id: 3, label: "Email", type: "email"},  {id: 4, label: "Date of Birth", type: "date"}];
   return (
     <AppContainer>
       <FormHeading/>
-      <FormElements formItems={formItems} />
-      <FormSubmit innerText='Submit'/>
+      { 
+        formOpen === "FORM" ? 
+        <>
+          <FormElements formItems={formItems} />
+          <FormSubmit innerText='Submit'/>
+        </>
+        :
+        <FormHome/>
+      }
+      <div className="flex gap-2">
+        <FormBtn onClick={() => {
+          setFormOpen("FORM");
+        }}
+        innerText="Open Form"/>
+
+        <FormBtn onClick={() => {
+          setFormOpen("HOME");
+        }}
+        innerText="Close Form"/>
+      </div>
+
     </AppContainer>
   );
 

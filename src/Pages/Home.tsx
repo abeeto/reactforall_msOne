2e18e5c313c30ae12b79eefe30a93e7670c04f30
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-03-17 12:35:54 +0530

    Add a live search feature

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,10 @@
-import { navigate } from "raviger";
-import { useState} from "react";
+import { navigate, useQueryParams } from "raviger";
+import { useEffect, useState} from "react";
 
 export default function Home() {
     const [selectedFormKey, setSelectedFormKey] = useState<string>("");
-
+    const [searchString, setSearchString] = useState<string>("");
+    const [{search}, setQuery] = useQueryParams();
     const handleFormSelect = (key  : string ) => {
         if (key) {
             if (key === selectedFormKey) {
@@ -41,12 +42,27 @@ export default function Home() {
         }
       };
     const [availableForms, setAvailableForms] = useState(Object.keys(localStorage));
+    useEffect(() => {
+        setQuery({string: searchString});
+    }, [searchString, setQuery])
     return (
         <div className="flex flex-col box-border mx-4">
             <div className="flex flex-col flex-1">
+                <form action="/" method="GET" className="flex flex-col" onSubmit={(e) => {
+                    e.preventDefault();
+                    setQuery({search: searchString});
+                }}>
+                    <label htmlFor="searchTitle">Search</label>
+                    <input className="border-2 border-grey-200 rounded-md p-2 flex-1" id="searchTitle" name="q" type="text" onChange={(e) => {
+                        setSearchString(e.target.value);
+                    }} />
+                </form>
                 <div className="text-lg mb-3">Available Forms:</div>
                 {
-                    availableForms.map((key) => {
+                    availableForms.filter((key => {
+                        let formObj = JSON.parse(localStorage.getItem(key) || "")
+                        return formObj.title.includes(searchString);
+                    })).map((key) => {
                         let item = JSON.parse(localStorage.getItem(key) as string);
                         return (
                             <div key={key} data-key={key} onClick={() => handleFormSelect(key)}  className={`text-xl font-thin mb-5 p-3 bg-gray-100 rounded-md flex-grow ${key === selectedFormKey && "border-2 border-blue-500"}`}>
